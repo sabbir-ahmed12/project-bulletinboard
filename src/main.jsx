f0fd49425c6a5f0a5c8ed66fde7917a5565b6ad1
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2023-04-14 15:22:54 +0600

    Added functionality to handle async requests using thunk.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider> 
+    </Provider>
   </React.StrictMode>
 );
